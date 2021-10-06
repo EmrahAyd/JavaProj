a36291acfc13f6fb6fccd7054e0015453920c4b8
Drea <dreamea@gmail.com>
2021-10-06 23:31:31 +0300

    new

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Selam dünyalı");
+    }
+int s ;
+int   a = 1112031584;
+while ( i = 1; i < (a); i++ );
+   {
+    if (a[i] % 2 == a[i-1] % 2) {
+        s += max(a[i], a[i-1])
+    }
+    }
+goto_url ('www.multisoft.se/')
+}
