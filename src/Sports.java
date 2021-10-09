66e2d486c9015330ba9eb0d5c25927ddde404784
Drea <dreamea@gmail.com>
2021-10-10 01:27:33 +0300

    Üs alma long ile

---
+++ src/Sports.java
@@ -0,0 +1,14 @@
+public class Sports {
+    public static void main(String[] args) {
+
+            String getName;
+            {
+
+                return "Generic Sports";
+            }
+            void getNumberOfTeamMembers(){
+                System.out.println( "Each team has n players in " + getName() );
+
+        }
+    }
+}
---
+++ src/StaticVoidvs.java
@@ -0,0 +1,18 @@
+public class StaticVoidvs {
+
+    static int sum(int a, int b) {
+
+
+        int c = a*b;
+        return c;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(sum(45,58));
+
+
+    }
+
+
+}
