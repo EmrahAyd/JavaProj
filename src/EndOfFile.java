1546e5c10f9fbcfe0aaeeb60b40a7b6a9fc9ed03
Drea <dreamea@gmail.com>
2021-10-12 02:17:34 +0300

    EndOfFile

---
+++ src/EndOfFile.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class EndOfFile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a;
+        int i =0;
+        do {
+
+            a = sc.nextLine();
+               System.out.println( i + " " + a);
+i++;
+
+                }
+        while ( !a.equals("yeter")) ;
+
+            }
+        }
+
+
+
+
